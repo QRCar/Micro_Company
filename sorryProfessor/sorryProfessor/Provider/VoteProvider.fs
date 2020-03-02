19ed246e196c7c99c6165c606202f971f123f245
antosox <antoine.burgaudeau@gmail.com>
2020-03-02 20:07:03 +0100

    master to dbo

--- sorryProfessor/sorryProfessor/Provider/VoteProvider.fs
+++ sorryProfessor/sorryProfessor/Provider/VoteProvider.fs
@@ -7,17 +7,17 @@ open sorryProfessor
 module getVote=
 
     // with nbvote
-    //Select id_billet, COUNT(*) as nbvote from master.user_billet
+    //Select id_billet, COUNT(*) as nbvote from dbo.user_billet
     //group by id_billet order by nbvote desc
-    let getNbVote = dapperQuery<Guid>"Select id_billet from master.user_billet
+    let getNbVote = dapperQuery<Guid>"Select id_billet from dbo.user_billet
                                         group by  id_billet order by COUNT(*) desc" |> Seq.toArray
 
-    let getLaterUser = dapperQuery<Guid>"Select id_user from master.billet
+    let getLaterUser = dapperQuery<Guid>"Select id_user from dbo.billet
                                         group by  id_user order by COUNT(*) desc" |> Seq.toArray
                                             
 
 module postVote=
     
-    let makeVote vote= dapperMapParameterizedQueryModel<unit> "insert into master.user_billet
+    let makeVote vote= dapperMapParameterizedQueryModel<unit> "insert into dbo.user_billet
                                     Values (@id_user, @id_billet)"
                                     (vote)
