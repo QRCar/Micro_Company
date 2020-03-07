1a4fe4c26c4b0a7e38116e9366a24055305f203e
antosox <antoine.burgaudeau@gmail.com>
2020-03-07 22:00:26 +0100

    add cors allow

--- sorryProfessor/sorryProfessor/Startup.fs
+++ sorryProfessor/sorryProfessor/Startup.fs
@@ -5,12 +5,20 @@ open System.Collections.Generic
 open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.HttpsPolicy;
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+
+module myCors =
+    let corsConfig (builder : CorsPolicyBuilder) =
+           builder.WithOrigins("https://localhost:5002")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+           |> ignore
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
@@ -21,6 +29,7 @@ type Startup private () =
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
         services.AddControllers() |> ignore
+        services.AddCors() |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
@@ -29,7 +38,9 @@ type Startup private () =
 
         app.UseHttpsRedirection() |> ignore
         app.UseRouting() |> ignore
-
+        
+        app.UseCors(myCors.corsConfig) |> ignore
+        
         app.UseAuthorization() |> ignore
 
         app.UseEndpoints(fun endpoints ->
